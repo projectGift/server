79f6b9992a24476147430affe94dae857db85b95
Eunsong Park <run.eunsong@gmail.com>
2023-01-27 20:30:51 +0900

    deploy test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM node:18
 RUN mkdir -p /var/app
 WORKDIR /var/app
-COPY . .
+COPY package.json ./
 RUN npm install
+COPY ./ ./
 RUN npm run build
 EXPOSE 3000
-CMD [ "node", "dist/main.js" ]
+CMD [ "npm", "run", "start" ]
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,4 +1,11 @@
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { RatingsRepository } from 'src/ratings/ratings.repository';
+import { UsersController } from './users.controller';
+import { UsersModule } from './users.module';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
@@ -10,9 +17,49 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+
+    // service.signUp({
+    //   email: 'hello123@gmail.com',
+    //   password: '1234',
+    // });
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // describe('signIn', () => {
+  //   it('should return a access token', () => {
+  //     const accessToken = service.signIn({
+  //       email: 'hello123@gmail.com',
+  //       password: '1234',
+  //     });
+  //     expect(accessToken).toBeDefined();
+  //     // expect(accessToken).toEqual();
+  //   });
+
+  //   it('should throw NotFoundException', () => {
+  //     try {
+  //       service.signIn({
+  //         email: 'nouser@gmail.com',
+  //         password: '4321',
+  //       });
+  //     } catch (e) {
+  //       expect(e).toBeInstanceOf(NotFoundException);
+  //       expect(e.message).toEqual('No user');
+  //     }
+  //   });
+
+  //   it('should throw UnauthorizedException', () => {
+  //     try {
+  //       service.signIn({
+  //         email: 'hello123@gmail.com',
+  //         password: '1111',
+  //       });
+  //     } catch (e) {
+  //       expect(e).toBeInstanceOf(UnauthorizedException);
+  //       expect(e.message).toEqual('Login failed');
+  //     }
+  //   });
+  // });
 });
